fe20b67bf7c351e101aa72cdc39dbb26f6b8d10f
KaratasD <dogukan.karatas@dlubal.com>
2024-07-03 12:43:20 +0200

    redirect

---
+++ redirect.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Set the title of the Streamlit app
+st.title('SeisKit has a new version now 🎉')
+st.write("Please visit our new adress 🔔")
+
+
+st.markdown('<a href="https://seiskitbeta.pythonanywhere.com/" target="">Go To SeisKit</a>', unsafe_allow_html=True)
